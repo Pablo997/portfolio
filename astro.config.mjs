6761087f20d6517492060ccdf3e136e4d77d5db2
Pablo Aragonés <pablo.aragones@3xs.es>
2026-04-28 12:09:24 +0200

    Add webcoreui integration and update portfolio layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { webcore } from 'webcoreui/integration';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [webcore()],
+});
